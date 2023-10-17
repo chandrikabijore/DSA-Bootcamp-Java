f5817e69ec0a4947aa2387d3ce72bbe4537cd3dd
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2023-10-17 15:57:52 +0530

    SumTwo.java I

---
+++ Assignments/01 - Flow of program/02 - SumTwo.java
@@ -0,0 +1,10 @@
+// Q2.Take two numbers and print the sum of both.
+
+package com.chandrika;
+public class SumTwo {
+    public static void main(String[] args) {
+        int N1 = 2, N2 = 4, Sum;
+        Sum = N1 + N2;
+        System.out.println("Sum of this numbers is : " +Sum);
+    }
+}
