706a7c64ec10092005ac72f3471e4a8ba2222c90
Chandrika Bijore <93921178+chandrikabijore@users.noreply.github.com>
2024-07-13 15:56:14 +0530

    Area_of_Parallelogram

---
+++ Assignments/03 - Conditions-Loops/DSA_03_K5_Area_of_Parallelogram.java
@@ -0,0 +1,15 @@
+// 5. Area Of Parallelogram
+package com.chandrika;
+
+public class DSA_03_K5_Area_of_Parallelogram
+{
+    public static void main(String[] args)
+    {
+        int base = 30;
+        int length = 40;
+
+        double AreaParallelogram = base * length;
+
+        System.out.println("Area of Parallelogram is : " + AreaParallelogram);         // 1200.0
+    }
+}
